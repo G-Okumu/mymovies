ae97e9dcf204f8617661fe8c9b68d49f0f64d741
George-Okumu <gokumu12@gmail.com>
2022-08-17 20:44:38 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // console.log(`${process.env.REACT_APP_API_KEY}`);
+  const [popular, setPopular] = useState([]);
+
+  console.log(popular);
+
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}upcoming?${process.env.REACT_APP_API_STRING}=${process.env.REACT_APP_API_KEY}`)
+    .then(r => r.json())
+    .then(data => {
+      setPopular(data);
+    });
+  }, [])
+
+  
   return (
     <div className="App">
       <header className="App-header">
